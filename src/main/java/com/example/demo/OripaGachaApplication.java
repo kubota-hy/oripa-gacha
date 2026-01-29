6e644bfe715e811f76f3a2045d0b49184b467353
vivid <vivid@namunamu>
2026-01-29 12:03:02 +0900

    add:test

---
+++ src/main/java/com/example/demo/OripaGachaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OripaGachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OripaGachaApplication.class, args);
+	}
+
+}
